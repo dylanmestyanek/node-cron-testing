f3d1b6413b0f37009f9c7dd3556f5cd3f310edd0
Dylan Mestyanek <mestyanekdylan@gmail.com>
2020-01-06 15:55:03 -0600

    trying stuff

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const server = require("express")();
+
+server.listen(process.env.PORT || 4000, () => console.log("\n === We're doing it!! === \n"))
+
 const cron = require("node-cron");
 
 cron.schedule("* * * * * *", () => {
