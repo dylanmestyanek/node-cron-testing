3429e6b325d3f9c07658e24c039669566c234524
Dylan Mestyanek <mestyanekdylan@gmail.com>
2020-01-17 22:13:54 -0600

    Removed cron

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 const server = require("express")();
 
 server.listen(process.env.PORT || 4000, () => console.log("\n === We're doing it!! === \n"))
-
-// const cron = require("node-cron");
-
-// cron.schedule("0 0 */1 * * *", () => {
-    console.count("test");
-// })
